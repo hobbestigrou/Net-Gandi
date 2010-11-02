cf1626e6864206b8aa982fe7ea48c0a8a0b77985
Natal Ngétal <hobbestigrou@erakis.im>
2010-11-02 11:11:34 +0100

    New version.

--- lib/Net/Gandi.pm
+++ lib/Net/Gandi.pm
@@ -20,7 +20,7 @@ Net::Gandi - A perl interface to the Gandi XMLRPC API
 
 =cut
 
-our $VERSION = '0.5';
+our $VERSION = '0.6';
 
 has 'apikey' => ( is       => 'rw', 
                   required => 1,
