629c8c431072c1e8528254e573bb4af3676b8171
Hobbestigrou <hobbestigrou@erakis.im>
2011-06-19 02:04:51 +0200

    [Core] Types: Add a new class for type definitions.

---
+++ lib/Net/Gandi/Types.pm
@@ -0,0 +1,38 @@
+package Net::Gandi::Types;
+
+use Moose::Util::TypeConstraints;
+use URI;
+
+=head1 NAME
+
+Net::Gandi::Types - Type definition for L<Net::Gandi>
+
+=head1 SYNOPSIS
+    use Net::Gandi::Types
+
+=head1 DESCRIPTION
+
+Moose type definitions for L<Net::Gandi>
+
+=cut
+
+=head1 Net::Gandi::Types::URI
+
+Subtype of L<URI>. Can be coerced from a string representing the URL.
+
+=cut
+
+subtype 'Net::Gandi::Types::URI' => as 'URI';
+
+coerce 'Net::Gandi::Types::URI' => from 'Str' => via {
+    URI->new($_);
+};
+
+1;
+
+=head1 SEE ALSO
+L<Moose::Util::TypeConstraints>
+
+=head1 AUTHOR
+
+Natal Ngétal
