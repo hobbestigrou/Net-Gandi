8b3274f187faa58709a4382b478733cc6ee7fdb0
Hobbestigrou <hobbestigrou@erakis.im>
2012-04-30 16:03:50 +0200

    [Core] Vm: Add documentation, and params validation.

--- lib/Net/Gandi/Hosting/VM.pm
+++ lib/Net/Gandi/Hosting/VM.pm
@@ -17,6 +17,17 @@ has client => (
     required => 1,
 );
 
+=method list
+
+  $vm->list;
+
+List virtual machines.
+
+  input: opts (HashRef) : Filtering options
+  output: (HashRef)     : List of vm
+
+=cut
+
 sub list {
     my ( $self, $params ) = validated_list(
         \@_,
@@ -26,6 +37,17 @@ sub list {
     $params ||= {};
     return $self->client->call_rpc( "vm.list", $params );
 }
+
+=method count
+
+  $vm->count;
+
+Count virtual machines.
+
+  input: opts (HashRef) : Filtering options
+  output: (Int)         : count of vm
+
+=cut
 
 sub count {
     my ( $self, $params ) = validated_list(
@@ -37,6 +59,17 @@ sub count {
     return $self->client->call_rpc('vm.count', $params);
 }
 
+=method info
+
+  $vm->info
+
+Get information about a virtual machine.
+
+  input: None
+  output: (HashRef) : Vm informations
+
+=cut
+
 sub info {
     my ( $self ) = @_;
 
@@ -44,10 +77,20 @@ sub info {
     return $self->client->call_rpc( 'vm.info', $self->id );
 }
 
+=method create_from
+
+Create a new virtual machine with respect to the attributes specified by vm_spec.
+Disk, iface, and vm must be in the same datacenter.
+
+  input: vm_spec (HashRef)   : specifications of the VM to create
+  output: (ArrayRef)         : Operation vm create and iface create
+
+=cut
+
 sub create {
     my ( $self, $params ) = validated_list(
         \@_,
-        opts => { isa => 'HashRef' }
+        vm_spec => { isa => 'HashRef' }
     );
 
     _validated_params('vm_create', $params);
@@ -59,8 +102,25 @@ sub create {
     return $self->client->call_rpc( "vm.create", $params );
 }
 
+=method create_from
+
+Create a disk and a virtual machine.
+This is a convenient method to do the disk.create and vm.create in a single API call.
+
+  input: vm_spec (HashRef)   : specifications of the VM to create
+         disk_spec (HashRef) : specifications of the Disk to create
+         src_disk_id (Int)   : source disk unique identifier
+  output: (ArrayRef)         : Operation vm create, disk create, and iface create
+
+=cut
+
 sub create_from {
-    my ( $self, $params, $disk_params, $src_disk_id ) = @_;
+    my ( $self, $params, $disk_spec, $src_disk_id ) = validated_list(
+        \@_,
+        vm_spec     => { isa => 'HashRef' },
+        disk_spec   => { isa => 'HashRef' },
+        src_disk_id => { isa => 'Int' }
+    );
 
     _validated_params('vm_create_from', $params);
 
@@ -70,13 +130,24 @@ sub create_from {
             ->value($params->{$param});
     }
 
-    return $self->client->call_rpc( "vm.create_from", $params, $disk_params, $src_disk_id );
+    return $self->client->call_rpc( "vm.create_from", $params, $disk_spec, $src_disk_id );
 }
+
+=method update
+
+  $vm->update;
+
+Update a virtual machine with respect to the attributes specified by update_spec.
+
+  input: update_spec (HashRef) : specifications of the virtual machine to update
+  output: (HashRef)  : Vm update operation
+
+=cut
 
 sub update {
     my ( $self, $params ) = validated_list(
         \@_,
-        opts => { isa => 'HashRef' }
+        update_spec => { isa => 'HashRef' }
     );
 
     carp 'Required parameter id is not defined' if ( ! $self->id );
@@ -85,8 +156,21 @@ sub update {
     return $self->client->call_rpc('vm.update', $self->id, $params);
 }
 
+=method disk_attach
+
+Attach a disk to a virtual machine.
+To access the disk data inside the VM, it MUST be attached to the VM.
+When options.position is 0, swaps position with current disk 0.
+A disk can be attached to only one VM.
+
+=cut
+
 sub disk_attach {
-    my ( $self, $disk_id, $params ) = @_;
+    my ( $self, $disk_id, $params ) = validated_list(
+        \@_,
+        disk_id => { isa => 'Int'},
+        opts    => { isa => 'HashRef' }
+    );
 
     carp 'Required parameter id is not defined' if ( ! $self->id );
 
@@ -97,6 +181,13 @@ sub disk_attach {
         return $self->client->call_rpc('vm.disk_attach', $self->id, $disk_id);
     }
 }
+
+=method disk_detach
+
+Detach a disk from a virtual machine.
+If the disk position is 0, i.e. the system disk, the virtual machine must be halted to detach the disk.
+
+=cut
 
 sub disk_detach {
     my ( $self, $disk_id ) = validated_list(
@@ -111,7 +202,7 @@ sub disk_detach {
 }
 
 
-=head1 start
+=method start
 
 Starts a VM and return the corresponding operation
 Parameter: None
@@ -125,10 +216,12 @@ sub start {
     return $self->client->call_rpc('vm.start', $self->id);
 }
 
-=head1 stop
+=method stop
 
 Stops a VM and returns the corresponding operation.
-Parameter: None
+
+  input: None
+  output: (HashRef): Operation vm stop
 
 =cut
 
@@ -139,10 +232,12 @@ sub stop {
     return $self->client->call_rpc('vm.stop', $self->id);
 }
 
-=head1 reboot
+=method reboot
 
 Reboots a VM and returns the corresponding operation
-Parameter: None
+
+  input: None
+  output: (HashRef): operation vm reboot
 
 =cut
 
@@ -153,11 +248,13 @@ sub reboot {
     return $self->client->call_rpc('vm.reboot', $self->id);
 }
 
-=head1
+=method delete
 
 Deletes a VM. Deletes the disk attached in position 0, the disk used as system disk.
 Also deletes the first network interface. Detach all extra disks and network interfaces.
-Parameter: None
+
+  input: None
+  output: (HashRef): Operation vm delete
 
 =cut
 
