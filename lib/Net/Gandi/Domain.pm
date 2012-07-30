41f2c42e9de240df53b9b72c281bfac299f660da
Hobbestigrou <hobbestigrou@erakis.im>
2012-07-30 22:49:51 +0200

    [Core] Gandi: Add new method count and info.

--- lib/Net/Gandi/Domain.pm
+++ lib/Net/Gandi/Domain.pm
@@ -6,6 +6,8 @@ use namespace::autoclean;
 
 use Net::Gandi::Types Client => { -as => 'Client_T' };
 
+use Carp;
+
 =attr apikey
 
 rw, Str. The domain name.
@@ -39,6 +41,46 @@ sub list {
 
     $params ||= {};
     return $self->client->api_call( "domain.list", $params );
+}
+
+=method count
+
+  $domain->count;
+
+Count domains associated to the contact represented by apikey.
+
+  input: opts (HashRef) : Filtering options
+  output: (Int)         : count of domain
+
+=cut
+
+sub count {
+    my ( $self, $params ) = validated_list(
+        \@_,
+        opts => { isa => 'HashRef', optional => 1 }
+    );
+
+    $params ||= {};
+    return $self->client->api_call('domain.count', $params);
+}
+
+=method info
+
+  $domain->info
+
+Get domain information.
+
+  input: None
+  output: (HashRef) : Domain information
+
+=cut
+
+sub info {
+    my ( $self ) = @_;
+
+    carp 'Required parameter domain attribute is not defined'
+        if ( ! $self->domain );
+    return $self->client->api_call( 'domain.info', $self->domain );
 }
 
 1;
